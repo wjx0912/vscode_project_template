2e1616f7669c963133da9a174525cda17baa1968
wjx0912 <80101277@qq.com>
2021-04-13 08:31:33 +0800

    参数ok

--- windows/python_conda/helloworld.py
+++ windows/python_conda/helloworld.py
@@ -1,5 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+
+print ('参数个数为:', len(sys.argv), '个参数。')
+print ('参数列表:', str(sys.argv))
 
 x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
