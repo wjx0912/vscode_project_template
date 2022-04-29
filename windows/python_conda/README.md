05fed4a2afc7f74bbe320d57d30c5365baaa435d
wyt <80101277@qq.com>
2022-04-29 17:51:55 +0800

    python conda vscode debug

--- windows/python_conda/README.md
+++ windows/python_conda/README.md
@@ -3,13 +3,4 @@
 https://zhuanlan.zhihu.com/p/30500765
 
 
-
-conda create -n py392  python=3.9.2
-
-activate py392
-
-
-
-pip install numpy
-
-pip install matplotlib
+pip install numpy matplotlib %douban%
--- windows/python_conda/helloworld.py
+++ windows/python_conda/helloworld.py
@@ -1,13 +1,21 @@
-import numpy as np
-import matplotlib.pyplot as plt
+#import numpy as np
+#import matplotlib.pyplot as plt
 import sys
+import os
 
+print(sys.version)
 print ('参数个数为:', len(sys.argv), '个参数。')
 print ('参数列表:', str(sys.argv))
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+env_dist = os.environ 
+ 
+# 打印所有环境变量，遍历字典
+for key in env_dist:
+    print(key + ' : ' + env_dist[key])
+
+#x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
+#plt.plot(x, np.sin(x))       # Plot the sine of each x point
+#plt.show()                   # Display the plot
 
 print("hello, world")
 print("hello, world")
